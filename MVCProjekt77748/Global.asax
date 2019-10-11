4d1bda5da51690d7f72e53151095b336d93afeca
Łukasz Kawecki <kawec94@gmail.com>
2019-10-11 12:54:08 +0200

    Add project files.

---
+++ MVCProjekt77748/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCProjekt77748.MvcApplication" Language="C#" %>
---
+++ MVCProjekt77748/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCProjekt77748.Startup))]
+namespace MVCProjekt77748
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
